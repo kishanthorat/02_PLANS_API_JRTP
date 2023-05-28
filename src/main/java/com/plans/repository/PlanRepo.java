a845a02a711779587051ff02ba03877ed244cbd6
User <kishanthorat5@gmail.com>
2023-05-28 16:17:27 +0530

    Add Missing @Repository Annotation

--- src/main/java/com/plans/repository/PlanRepo.java
+++ src/main/java/com/plans/repository/PlanRepo.java
@@ -1,9 +1,10 @@
 package com.plans.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.plans.entity.Plan;
-
+@Repository
 public interface PlanRepo extends JpaRepository<Plan, Integer>{
 
 }
